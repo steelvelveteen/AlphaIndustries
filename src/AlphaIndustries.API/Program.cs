960f7388c49eea569bcf3694567347af5f246ca9
steelvelveteen <joeyvico@gmail.com>
2023-04-21 10:35:53 +0200

    Added MediatR to API project, pending addition of package to the Application layer

--- src/AlphaIndustries.API/Program.cs
+++ src/AlphaIndustries.API/Program.cs
@@ -1,4 +1,4 @@
-using MediatR;
+using System.Reflection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(typeof(Program));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
 
 // Configure Serilog
